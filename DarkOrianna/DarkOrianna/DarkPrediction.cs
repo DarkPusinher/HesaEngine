167eab1f14a435878526383036f8f84531e4c3d6
DarkPusinher <atessu16@hotmail.com>
2017-06-20 18:59:03 +0300

    Improved Prediction!

--- DarkOrianna/DarkOrianna/DarkPrediction.cs
+++ DarkOrianna/DarkOrianna/DarkPrediction.cs
@@ -1,13 +1,9 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-using HesaEngine.SDK;
 using SharpDX;
+using System.Linq;
+using HesaEngine.SDK;
 using HesaEngine.SDK.GameObjects;
-using static DarkOrianna.MenuManager;
-using static DarkOrianna.SpellManager;
+using System.Collections.Generic;
 
 namespace DarkOrianna
 {
@@ -28,6 +24,8 @@ namespace DarkOrianna
 
         public static List<Vector3> epos = new List<Vector3>();
         public static List<Vector3> eposC = new List<Vector3>();
+        public static List<Vector3> dir = new List<Vector3>();
+        public static List<Vector3> dirC = new List<Vector3>();
         public static List<double> travelDistances = new List<double>();
         public static double delayer = 0;
         public static double delayer1 = 0;
@@ -82,122 +80,132 @@ namespace DarkOrianna
             float t = 0;
             List<double> Ds = new List<double>();
 
-            if (enemy != null && !enemy.IsDead && enemy.IsValidTarget(spell.Range))
+            if (enemy != null)
             {
-                if (!enemy.IsMoving)
+                if (!enemy.IsDead)
                 {
-                    stoper = false;
-                    return enemy.Position;
-                }
+                    //if (!enemy.IsMoving)
+                    //{
+                    //    stoper = false;
+                    //    return enemy.Position;
+                    //}
 
 
-                if (enemy.IsDashing())
-                {
-                    
-                    stoper = false;
-                    return empt;
-                }
-
-                float ES = enemy.MovementSpeed;
-
-                if (Game.GameTimeTickCount >= delayer)
-                {
-                    
-                    epos.Add(enemy.Position);
-                    delayer = Game.GameTimeTickCount + 10;
-                }
-
-                //if (epos.size() > 3 && stoper == false)
-                //{
-                //    if (ToVec2((epos[epos.size() - 2] - epos[epos.size() - 3]).VectorNormalize()) != ToVec2((epos[epos.size() - 3] - epos[epos.size() - 4]).VectorNormalize()))
-                //    {
-                //        Vec2 loc1 = ToVec2(enemy->GetPosition() + ((epos[epos.size() - 2] - epos[epos.size() - 3]).VectorNormalize())) * 1000;
-                //        Vec2 loc2 = ToVec2(enemy->GetPosition() + ((epos[epos.size() - 3] - epos[epos.size() - 4]).VectorNormalize())) * 1000;
-                //        if (Distance(loc1, loc2) > 10)
-                //        {
-                //            stoper = true;
-                //            delayer2 = GGame->TickCount() + 300;
-                //        }
-                //        else
-                //        {
-                //            return empt;
-                //        }
-                //    }
-                //    else
-                //    {
-                //        return empt;
-                //    }
-                //}
-                //
-                //if (stoper = true && GGame->TickCount() <= delayer2)
-                //{
-                //    Vec3 direction = (epos[epos.size() - 1] - epos[epos.size() - 2]).VectorNormalize();
-                //    double D = 0;
-                //    h = BallLocation.x;
-                //    k = BallLocation.y;
-                //
-                //    for (int i = 0; i < 6000; i++)
-                //    {
-                //        Vec3 EF = enemy->GetPosition() + direction * (ES / 1000) * i;
-                //        D = abs(sqrt(pow((EF.x - h), 2) + pow(EF.y - k, 2)) - (SS / 1000) * i);
-                //        Ds.push_back(D);
-                //        if (Ds.size() > 0)
-                //        {
-                //            if (Ds[Ds.size() - 1] <= temporar)
-                //            {
-                //                temporar = Ds[Ds.size() - 1];
-                //            }
-                //            else
-                //            {
-                //                t = i - 1;
-                //                break;
-                //            }
-                //        }
-                //    }
-                //    t = (t + (Q->GetDelay() * 1000) + (GGame->Latency() / 1));
-                //    Vec3 EFuture = enemy->GetPosition() + direction * (ES / 1000) * t;
-                //    Vec3 fut = Extend(EFuture, enemy->GetPosition(), (Q->Radius() - 30));
-                //    return fut;
-                //}
-                //else
-                //{
-                //    return empt;
-                //}
-
-                if (epos.Count > 1)
-                {
-
-                    Vector3 direction = (epos[epos.Count - 1] - epos[epos.Count - 2]).Normalized();
-                    double D = 0;
-                    h = skillInitialPosition.X;
-                    k = skillInitialPosition.Z;
-                    //float xvar = direction.X*(ES / 1000);
-                    //float yvar = direction.Y * (ES / 1000);
-                    //float zvar = direction.Z * (ES / 1000);
-                    //Vector3 dir = new Vector3();
-                    //dir.X = xvar;
-                    //dir.Y = yvar;
-                    //dir.Z = zvar;
-                    
-                    for (int i = 0; i < 6000; i += 10)
+                    if (enemy.IsDashing())
                     {
-                        Vector3 EF = enemy.Position + (direction * (ES / 1000) * i);
-                        D = Math.Abs(Math.Sqrt(Math.Pow((EF.X - h), 2) + Math.Pow(EF.Z - k, 2)) - (SS / 1000) * i);
-                        Ds.Add(D);
-                        if (Ds[Ds.Count - 1] <= temporar)
-                        {
-                            temporar = Ds[Ds.Count - 1];
-                        }
-                        else
-                        {
-                            t = i - 1;
-                            break;
-                        }
+
+                        stoper = false;
+                        return empt;
                     }
-                    t = t + (spell.Delay * 1000) + (Game.Ping / 1);
-                    Vector3 EFuture = enemy.Position + (direction * (ES / 1000) * t);
-                    Vector3 fut = EFuture.Extend(enemy.Position, (spell.Width - 30));
-                    return fut;
+
+                    float ES = enemy.MovementSpeed;
+
+                    if (Game.GameTimeTickCount >= delayer)
+                    {
+
+                        epos.Add(enemy.Position);
+                        delayer = Game.GameTimeTickCount + 10;
+                    }
+
+
+                    //if (epos.size() > 3 && stoper == false)
+                    //{
+                    //    if (ToVec2((epos[epos.size() - 2] - epos[epos.size() - 3]).VectorNormalize()) != ToVec2((epos[epos.size() - 3] - epos[epos.size() - 4]).VectorNormalize()))
+                    //    {
+                    //        Vec2 loc1 = ToVec2(enemy->GetPosition() + ((epos[epos.size() - 2] - epos[epos.size() - 3]).VectorNormalize())) * 1000;
+                    //        Vec2 loc2 = ToVec2(enemy->GetPosition() + ((epos[epos.size() - 3] - epos[epos.size() - 4]).VectorNormalize())) * 1000;
+                    //        if (Distance(loc1, loc2) > 10)
+                    //        {
+                    //            stoper = true;
+                    //            delayer2 = GGame->TickCount() + 300;
+                    //        }
+                    //        else
+                    //        {
+                    //            return empt;
+                    //        }
+                    //    }
+                    //    else
+                    //    {
+                    //        return empt;
+                    //    }
+                    //}
+                    //
+                    //if (stoper = true && GGame->TickCount() <= delayer2)
+                    //{
+                    //    Vec3 direction = (epos[epos.size() - 1] - epos[epos.size() - 2]).VectorNormalize();
+                    //    double D = 0;
+                    //    h = BallLocation.x;
+                    //    k = BallLocation.y;
+                    //
+                    //    for (int i = 0; i < 6000; i++)
+                    //    {
+                    //        Vec3 EF = enemy->GetPosition() + direction * (ES / 1000) * i;
+                    //        D = abs(sqrt(pow((EF.x - h), 2) + pow(EF.y - k, 2)) - (SS / 1000) * i);
+                    //        Ds.push_back(D);
+                    //        if (Ds.size() > 0)
+                    //        {
+                    //            if (Ds[Ds.size() - 1] <= temporar)
+                    //            {
+                    //                temporar = Ds[Ds.size() - 1];
+                    //            }
+                    //            else
+                    //            {
+                    //                t = i - 1;
+                    //                break;
+                    //            }
+                    //        }
+                    //    }
+                    //    t = (t + (Q->GetDelay() * 1000) + (GGame->Latency() / 1));
+                    //    Vec3 EFuture = enemy->GetPosition() + direction * (ES / 1000) * t;
+                    //    Vec3 fut = Extend(EFuture, enemy->GetPosition(), (Q->Radius() - 30));
+                    //    return fut;
+                    //}
+                    //else
+                    //{
+                    //    return empt;
+                    //}
+
+                    if (epos.Count > 1)
+                    {
+                        if (epos[epos.Count - 1] == epos[epos.Count - 2])
+                        {
+                            return enemy.Position;
+                        }
+
+                        Vector3 direction = (epos[epos.Count - 1] - epos[epos.Count - 2]).Normalized();
+                        double D = 0;
+                        h = skillInitialPosition.X;
+                        k = skillInitialPosition.Z;
+                        //float xvar = direction.X*(ES / 1000);
+                        //float yvar = direction.Y * (ES / 1000);
+                        //float zvar = direction.Z * (ES / 1000);
+                        //Vector3 dir = new Vector3();
+                        //dir.X = xvar;
+                        //dir.Y = yvar;
+                        //dir.Z = zvar;
+
+                        for (int i = 0; i < 6000; i += 10)
+                        {
+                            Vector3 EF = enemy.Position + (direction * (ES / 1000) * i);
+                            D = Math.Abs(Math.Sqrt(Math.Pow((EF.X - h), 2) + Math.Pow(EF.Z - k, 2)) - (SS / 1000) * i);
+                            Ds.Add(D);
+                            if (Ds[Ds.Count - 1] <= temporar)
+                            {
+                                temporar = Ds[Ds.Count - 1];
+                            }
+                            else
+                            {
+                                t = i - 1;
+                                break;
+                            }
+                        }
+                        t = t + (spell.Delay * 1000) + (Game.Ping / 1);
+                        Vector3 EFuture = enemy.Position + (direction * (ES / 1000) * t);
+                        Vector3 fut = EFuture.Extend(enemy.Position, (spell.Width - 30));
+                        return fut;
+                    }
+                    else
+                        return empt;
                 }
                 else
                     return empt;
@@ -209,8 +217,8 @@ namespace DarkOrianna
 
         public static Vector3 BestCastPosition(Spell spell, int targetHit, int radiusSpell) // Finds Best Cast position of a skill
         {
-            
-            var enemies = ObjectManager.Heroes.Enemies.Where(enemy => enemy != null & enemy.IsValidTarget(Q.Range));
+
+            var enemies = ObjectManager.Heroes.Enemies.Where(enemy => enemy != null & enemy.IsValidTarget(spell.Range));
             int count = 0;
             List<AIHeroClient> bestEnemies = new List<AIHeroClient>();
             float Xs = 0;
@@ -219,13 +227,13 @@ namespace DarkOrianna
             empt.Y = 0;
             empt.Z = 0;
 
-            
+
             foreach (var enemt in enemies)
             {
                 count = 0;
                 foreach (var ene in enemies)
                 {
-                        
+
                     if (enemt.Position.Distance(ene.Position) <= 2 * radiusSpell)
                     {
                         count += 1;
@@ -239,7 +247,7 @@ namespace DarkOrianna
 
             if (bestEnemies.Count >= targetHit && bestEnemies.Count > 1)
             {
-                
+
                 for (int k = 0; k < bestEnemies.Count; k++)
                 {
                     Xs += bestEnemies[k].Position.X;
@@ -261,7 +269,7 @@ namespace DarkOrianna
                 return empt;
         }  // 
 
-        public static Vector3 CirclerPrediction(Spell spell, AIHeroClient enemy)
+        public static Vector3 CirclerPrediction(Spell spell, AIHeroClient enemy, int hitChance) // hitChance 0 = medium, 1 = high
         {
             var player = ObjectManager.Me;
 
@@ -269,37 +277,123 @@ namespace DarkOrianna
             empt.Y = 0;
             empt.Z = 0;
 
-            if (enemy != null && !enemy.IsDead && enemy.IsValidTarget(spell.Range))
+            if (enemy != null)
             {
-                if (!enemy.IsMoving)
-                {
-                    stoperC = false;
-                    return enemy.Position;
-                }
-
-
-                if (enemy.IsDashing())
+                if (!enemy.IsDead)
                 {
 
-                    stoperC = false;
-                    return empt;
-                }
+                    //if (!enemy.IsMoving)
+                    //{
+                    //    
+                    //
+                    //    stoperC = false;
+                    //    return enemy.Position;
+                    //        
+                    //
+                    //}
 
-                float ES = enemy.MovementSpeed;
 
-                if (Game.GameTimeTickCount >= delayerC)
-                {
+                    if (enemy.IsDashing())
+                    {
 
-                    eposC.Add(enemy.Position);
-                    delayerC = Game.GameTimeTickCount + 10;
-                }
-                if (eposC.Count > 1)
-                {
+                        stoperC = false;
+                        return empt;
+                    }
 
-                    Vector3 direction = (eposC[eposC.Count - 1] - eposC[eposC.Count - 2]).Normalized();
-                    Vector3 EFuture = enemy.Position + (direction * (ES / 1000) * ((spell.Delay*1000) + Game.Ping));
-                    Vector3 fut = EFuture.Extend(enemy.Position, (spell.Width - 30));
-                    return fut;
+                    float ES = enemy.MovementSpeed;
+
+                    if (Game.GameTimeTickCount >= delayerC)
+                    {
+
+                        eposC.Add(enemy.Position);
+                        delayerC = Game.GameTimeTickCount + 10;
+                    }
+
+                    if (eposC.Count > 0)
+                    {
+                        if (eposC[eposC.Count - 1] == eposC[eposC.Count - 2])
+                        {
+                            stoperC = false;
+                            return enemy.Position;
+                        }
+                    }
+
+                    if (hitChance == 1)
+                    {
+                        if (eposC.Count > 3 && stoperC == false)
+                        {
+                            if (((eposC[eposC.Count - 2] - eposC[eposC.Count - 3]).Normalized()) != ((eposC[eposC.Count - 3] - eposC[eposC.Count - 4]).Normalized()))
+                            {
+                                Vector3 loc1 = (enemy.Position + ((eposC[eposC.Count - 2] - eposC[eposC.Count - 3]).Normalized())) * 1000;
+                                Vector3 loc2 = (enemy.Position + ((eposC[eposC.Count - 3] - eposC[eposC.Count - 4]).Normalized())) * 1000;
+                                if (loc1.Distance(loc2) > 10)
+                                {
+                                    stoperC = true;
+                                    delayer2 = Game.GameTimeTickCount + 300;
+                                }
+                                else
+                                {
+                                    return empt;
+                                }
+                            }
+                            else
+                            {
+                                return empt;
+                            }
+                        }
+                        if (stoperC = true && Game.GameTimeTickCount <= delayer2)
+                        {
+                            Vector3 direction = (eposC[eposC.Count - 1] - eposC[eposC.Count - 2]).Normalized();
+
+                            Vector3 EFuture = enemy.Position + direction * (ES / 1000) * (spell.Delay * 1000) + (Game.Ping / 1);
+                            Vector3 fut = EFuture.Extend(enemy.Position, (spell.Width - 30));
+                            return fut;
+                        }
+                        else
+                        {
+                            stoperC = false;
+                            return empt;
+                        }
+                    }
+
+
+
+                    //if (eposC.Count > 3 && stoper == false)
+                    //{
+                    //    if (((eposC[eposC.Count - 2] - eposC[eposC.Count - 3]).Normalized()) != ((eposC[eposC.Count - 3] - eposC[eposC.Count - 4]).Normalized()))
+                    //    {
+                    //        Vector3 loc1 = (enemy.Position + ((eposC[eposC.Count - 2] - eposC[eposC.Count - 3]).Normalized())) * 1000;
+                    //        Vector3 loc2 = (enemy.Position + ((eposC[eposC.Count - 3] - eposC[eposC.Count - 4]).Normalized())) * 1000;
+                    //        if (loc1.Distance(loc2) > 10)
+                    //        {
+                    //            stoper = true;
+                    //            delayer2 = Game.GameTimeTickCount + 300;
+                    //        }
+                    //        else
+                    //        {
+                    //            return empt;
+                    //        }
+                    //    }
+                    //    else
+                    //    {
+                    //        return empt;
+                    //    }
+                    //}
+
+
+                    else
+                    {
+                        if (eposC.Count > 1)
+                        {
+
+                            Vector3 direction = (eposC[eposC.Count - 1] - eposC[eposC.Count - 2]).Normalized();
+                            Vector3 EFuture = enemy.Position + (direction * (ES / 1000) * ((spell.Delay * 1000) + Game.Ping));
+                            Vector3 fut = EFuture.Extend(enemy.Position, (spell.Width - 30));
+                            return fut;
+                        }
+                        else
+                            return empt;
+                    }
                 }
                 else
                     return empt;
